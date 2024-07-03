a113b8f08476cb5f2015b332e274af54087eb802
Philipp <philipp.wiesenbach@uni-heidelberg.de>
2024-07-03 13:01:06 +0200

    update

---
+++ README.md
@@ -0,0 +1,59 @@
+# `rna_protein_saluki`: A plugin to run bioinformatical CNN-RNN Models.
+
+This projects implements fine-tuning of the [Saluki](https://genomebiology.biomedcentral.com/articles/10.1186/s13059-022-02811-x) for regressing half lives of RNA and protein sequences. In addition, it supports the extraction of leave-one-out (LOO) scores for fine-tuned models to analyse importance scores of individual inputs.
+
+In detail, the following steps are implemented:
+
+- Tokenization of RNA/Protein sequences via one-hot encoding of molecules.
+- Fine-tune models for regression.
+- Calculation of leave-one-out scores for you fine-tuned model.
+
+## Installation
+
+First clone the repo and cd into it. Then, we recommend to create a dedicated environment ([python venv](https://docs.python.org/3/library/venv.html)) for the project. Now, you install the project via the [Pipfile](./Pipfile) file which in turn will install the [biolm_utils](https://github.com/dieterich-lab/biolm_utils) library. Summarising, excute the following steps:
+
+```bash
+git clone https://github.com/dieterich-lab/rna_protein_saluki.git
+cd rna_protein_saluki
+python3 -m venv biolm_saluki 
+. biolm/biolm_saluki/activate
+pip install pipenv
+pipenv install
+```
+
+## File structure
+
+```bash
+├── exampleconfigs # exampleconfigs to work with
+├── Pipfile # installation file
+├── README.md
+├── rna_cnn_dataset.py # Implementation of the `RNABaseDataset`, espcially implementing the `__getitem__()` method.
+├── rna_cnn_models.py # Implementation of the models, espcially implementing the `getconfig()` method.
+├── saluki.py # Main script importing the `run()` function from `biolm_utils` and declaration of the model/data/training configuration.
+```
+
+## Usage
+
+The main script is [saluki.py](./saluki.py) which imports the `run()` function from the [biolm._utils](https://github.com/dieterich-lab/biolm_utils) library and provides the a custom `Config` object suitable for running the saluki model. The script can be run via
+
+```bash
+python saluki.py [tokenize | pre-train | fine-tune | predict | interepret]
+```
+
+To get a verbose exlplanation on all the possible parameters you can run the following:
+
+```bash
+python saluki.py -h 
+```
+
+For general usage and information about the configuration parameters we refer user to the [README](https://github.com/dieterich-lab/biolm_utils/blob/main/README.md) of the `biolm_utils` framework.
+
+## Example config files
+
+We offer two types of config files. The first one is for the pipeline of **tokenization**, **pre-training** (language models only), **fine-tuning**, **testing** (testing is also done during fine-tuning, but can be also again separately invoked) and extracting loo scores. The other one is for **inference** (getting predictions on new files) and **interpret** modes. The latter one are noticeably smaller as all the training cofigurations fall away.
+
+```bash
+exampleconfigs
+├── inference_interpret.yaml
+├── tokenize_pe-train_fine_tune_test_interpret.yaml
+```
---
+++ rna_cnn_models.py
@@ -0,0 +1,165 @@
+import torch.nn as nn
+from transformers import PreTrainedModel
+
+
+class MidBlock(nn.Module):
+    def __init__(
+        self,
+        input_channels,
+        output_channels,
+        kernel_width,
+        maxpool_width,
+        maxpool_stride,
+        dropout,
+    ):
+        super().__init__()
+        self.conv = nn.Conv1d(
+            in_channels=input_channels,
+            out_channels=output_channels,
+            kernel_size=kernel_width,
+        )
+        self.layernorm = nn.LayerNorm(normalized_shape=output_channels)
+        self.dropout = nn.Dropout(p=dropout)
+        self.maxpool = nn.MaxPool1d(kernel_size=maxpool_width, stride=maxpool_stride)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = x.permute(0, 2, 1)
+        x = self.layernorm(x)
+        x = x.permute(0, 2, 1)
+        x = self.dropout(x)
+        x = self.maxpool(x)
+        x = self.relu(x)
+        return x
+
+
+class EntryBlock(nn.Module):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel_width,
+    ):
+        super().__init__()
+        self.conv = nn.Conv1d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_width,
+        )
+        self.layernorm = nn.LayerNorm(normalized_shape=out_channels)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = x.permute(0, 2, 1)
+        x = self.layernorm(x)
+        x = x.permute(0, 2, 1)
+        x = self.relu(x)
+        return x
+
+
+class DenseBlock(nn.Module):
+    def __init__(self, channels, dropout, out_features):
+        super().__init__()
+        self.l1 = nn.Linear(in_features=channels, out_features=channels)
+        self.dropout = nn.Dropout(p=dropout)
+        self.batch_norm = nn.BatchNorm1d(num_features=channels)
+        self.relu = nn.ReLU()
+        self.l2 = nn.Linear(in_features=channels, out_features=out_features)
+
+    def forward(self, x):
+        x = self.l1(x)
+        x = self.dropout(x)  # batch x 64
+        x = self.batch_norm(x)
+        x = self.relu(x)
+        x = self.l2(x)
+        return x
+
+
+class GruBlock(nn.Module):
+    def __init__(self, channels):
+        super().__init__()
+        self.gru = nn.GRU(
+            input_size=channels,
+            hidden_size=channels,
+            batch_first=True,
+            bidirectional=False,
+        )
+        self.batch_norm = nn.BatchNorm1d(num_features=channels)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        _, x = self.gru(x)  # 1 x batch x 64
+        x = x.squeeze(0)  # batch x 64
+        x = self.batch_norm(x)
+        x = self.relu(x)
+        return x
+
+
+class Saluki(nn.Module):
+    def __init__(
+        self,
+        input_size,
+        channels=64,
+        kernel_width=5,
+        maxpool_width=2,
+        maxpool_stride=2,
+        layers=6,
+        num_labels=1,
+        dropout=0.3,
+    ):
+        super().__init__()
+        self.entry_block = EntryBlock(
+            in_channels=input_size, out_channels=channels, kernel_width=kernel_width
+        )
+        self.mid_blocks = nn.ModuleList(
+            [
+                MidBlock(
+                    channels,
+                    channels,
+                    kernel_width,
+                    maxpool_width,
+                    maxpool_stride,
+                    dropout,
+                )
+                for _ in range(layers)
+            ]
+        )
+        self.gru_block = GruBlock(channels=channels)
+        self.dense_block = DenseBlock(
+            channels=channels, dropout=dropout, out_features=num_labels
+        )
+
+    def forward(self, x):
+        x = x.permute(0, 2, 1)
+        x = self.entry_block(x)
+        for block in self.mid_blocks:
+            x = block(x)
+        x = x.permute(0, 2, 1)
+        x = x.flip(1)  # reverse direction
+        x = self.gru_block(x)
+        x = self.dense_block(x)
+        return x
+
+
+class HFSaluki(PreTrainedModel):
+    def __init__(self, config):
+        # def __init__(self, config, input_size):
+        super().__init__(config)
+        self.model = Saluki(input_size=config.input_size)
+
+    def forward(self, **kwargs):
+        x = kwargs["input_ids"]
+        x = self.model(x)
+        return {"logits": x}
+
+    @staticmethod
+    def get_config(args, config_cls, tokenizer, dataset, nlabels):
+        config = config_cls(
+            vocab_size=len(tokenizer),
+            max_position_embeddings=args.blocksize,
+            pad_token_id=tokenizer.pad_token_id,
+        )
+        config.input_size = dataset.OHE.categories_[0].size + dataset.nspecs
+        return config
