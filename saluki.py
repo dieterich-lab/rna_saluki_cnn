a6d1584a0bbcb1d3a6f4937a7ead60edbcfcc964
Philipp <philipp.wiesenbach@uni-heidelberg.de>
2024-07-03 13:01:59 +0200

    update

---
+++ saluki.py
@@ -0,0 +1,29 @@
+from biolm_utils.config import Config, set_config
+from transformers import BertConfig, DefaultDataCollator, PreTrainedTokenizerFast
+from transformers.image_processing_utils import BaseImageProcessor
+
+from rna_cnn_dataset import RNACNNDataset
+from rna_cnn_models import HFSaluki
+
+params = [
+    None,  # 0
+    HFSaluki,  # 1
+    PreTrainedTokenizerFast,  # 2
+    RNACNNDataset,  # 3
+    1e-3,  # 4
+    0.4,  # 5
+    0.001,  # 6
+    BaseImageProcessor,  # 7
+    None,  # 8
+    DefaultDataCollator,  # 9
+    False,  # 10
+    BertConfig,  # 11
+    False,  # 12
+]
+
+config = Config(*params)
+set_config(config)
+
+from biolm_utils.biolm import run
+
+run()
