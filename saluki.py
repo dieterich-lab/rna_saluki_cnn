9751855ba9b5baca8146d3fad191d9a6e05783b7
Philipp <philipp.wiesenbach@uni-heidelberg.de>
2024-07-08 12:23:21 +0200

    Refactoring dataset options fully into `biolm_utils`.

--- saluki.py
+++ saluki.py
@@ -2,14 +2,14 @@ from biolm_utils.config import Config, set_config
 from transformers import BertConfig, DefaultDataCollator, PreTrainedTokenizerFast
 from transformers.image_processing_utils import BaseImageProcessor
 
-from rna_cnn_dataset import RNACNNDataset
+# from rna_cnn_dataset import RNACNNDataset
 from rna_cnn_models import HFSaluki
 
 params = [
     None,  # 0
     HFSaluki,  # 1
     PreTrainedTokenizerFast,  # 2
-    RNACNNDataset,  # 3
+    # RNACNNDataset,  # 3
     1e-3,  # 4
     0.4,  # 5
     0.001,  # 6
