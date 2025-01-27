b4ca4c74a95a039882db42e8629201324151aede
Philipp <philipp.wiesenbach@uni-heidelberg.de>
2025-01-27 15:08:42 +0100

    Moved One-hot-encoding and tensorfication of the CNN dataset to the `__getitem__` method.

--- rna_cnn_dataset.py
+++ rna_cnn_dataset.py
@@ -1,10 +1,23 @@
 import numpy as np
 import torch
+from sklearn.preprocessing import OneHotEncoder
 
 from biolm_utils.rna_datasets import RNABaseDataset
 
 
 class RNACNNDataset(RNABaseDataset):
+    def __init__(self, **args):
+        super().__init__(**args)
+
+        # Define a one-hot encoder
+        non_special_vocab = [
+            v
+            for k, v in self.tokenizer.vocab.items()
+            if k not in self.tokenizer.special_tokens_map.values()
+        ]
+        self.OHE = OneHotEncoder(handle_unknown="ignore", sparse_output=False)
+        self.OHE.fit([[x] for x in non_special_vocab])
+
     def __getitem__(self, i):
         example = self.examples[i].copy()
         example["input_ids"] = self.OHE.transform(
